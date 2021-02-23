f026c108906f56864ae19983eca51a368d154646
Hal Fulcher <hallam.fulcher@gmail.com>
2021-02-23 23:09:36 +0000

    Nav bar started

---
+++ src/components/App.js
@@ -0,0 +1,14 @@
+import "../styles/App.css";
+import React from "react";
+import NavBar from "./NavBar";
+
+function App() {
+  return (
+    <div className="App">
+      <h2>Surreal Estate</h2>
+      <NavBar />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/NavBar.js
@@ -0,0 +1,16 @@
+import React from "react";
+import "../styles/NavBar.css";
+
+const NavBar = () => {
+  return (
+    <div className="nav-bar">
+      <p>Hi</p>
+      <img
+        src="https://mcrcodes.s3.eu-west-2.amazonaws.com/course/surreal-estate/logo.png"
+        alt="surreal-estate-logo"
+      />
+    </div>
+  );
+};
+
+export default NavBar;
