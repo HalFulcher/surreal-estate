68bf6a39e722585975796658ff3269c5347cf157
Hal Fulcher <hallam.fulcher@gmail.com>
2021-02-25 21:36:43 +0000

    Setting up routes

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,19 @@
-import "../styles/App.css";
 import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import "../styles/App.css";
 import NavBar from "./NavBar";
+import Properties from "./Properties";
 
 function App() {
   return (
-    <div className="App">
-      <h2>Surreal Estate</h2>
-      <NavBar />
-    </div>
+    <BrowserRouter>
+      {" "}
+      <div className="App">
+        <h2>Surreal Estate</h2>
+        <NavBar />
+        <Switch path />
+      </div>
+    </BrowserRouter>
   );
 }
 
--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,11 +4,20 @@ import "../styles/NavBar.css";
 const NavBar = () => {
   return (
     <div className="nav-bar">
-      <p>Hi</p>
       <img
         src="https://mcrcodes.s3.eu-west-2.amazonaws.com/course/surreal-estate/logo.png"
         alt="surreal-estate-logo"
       />
+      <div className="navbar-links">
+        <ul>
+          <li className="navbar-links-item" to="/">
+            View Properties
+          </li>
+          <li className="navbar-links-item" to="/">
+            Add a Property
+          </li>
+        </ul>
+      </div>
     </div>
   );
 };
