a3d7a36682ca1cfebe96c86d94505fe924f89f9e
Hal Fulcher <hallam.fulcher@gmail.com>
2021-03-07 18:49:37 +0000

    Retrieve properties

--- src/components/Properties.js
+++ src/components/Properties.js
@@ -1,7 +1,32 @@
-import React from "react";
+/* eslint-disable */
+
+import React, { useEffect, useState } from "react";
+import axios from "axios";
+import Alert from "./Alert";
+// import PropertyCard from "./PropertyCard";
 
 const Properties = () => {
-  return <div>Properties Page</div>;
+  const [properties, setProperties] = useState([]);
+  const [alert, setAlert] = useState({ message: "" });
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:4000/api/v1/PropertyListing")
+      .then((res) => {
+        console.log(res.data);
+        setProperties = res.data;
+      })
+      .catch(() => {
+        setAlert({
+          message: "Server error. Please try again later",
+          isSuccess: false,
+        });
+      });
+  }, []);
+
+  if (!alert.isSuccess) {
+    return <Alert message={alert.message} success={alert.isSuccess} />;
+  }
 };
 
 export default Properties;
